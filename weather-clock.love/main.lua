076377a51c3cff05d3250c63c011c14b948264eb
Subhojit Basu <subhojit.basu@gmail.com>
2019-06-26 09:38:42 +0530

    Changed Layout

--- weather-clock.love/main.lua
+++ weather-clock.love/main.lua
@@ -4,15 +4,15 @@ local ltn12 = require("ltn12")
 require("./view")
 
 function love.load()
-	success = love.window.setMode(640,480,{fullscreen=false,vsync=true,resizable=flase})
+	success = love.window.setMode(480,320,{fullscreen=false,vsync=true,resizable=flase})
 	T=11
 	lasttime = 0
 	weather_url = "http://api.openweathermap.org/data/2.5/weather?lat=12.88&lon=77.58&APPID=222bc56201e319d82a0c478a10ccb03c"
 	response ={}
-	carbon_font_20 = love.graphics.newFont("carbon_bl.ttf",20)
-	carbon_font_40 = love.graphics.newFont("carbon_bl.ttf",40)
-	carbon_font_60 = love.graphics.newFont("carbon_bl.ttf",60)
-	carbon_font_80 = love.graphics.newFont("carbon_bl.ttf",80)
+	carbon_font_20 = love.graphics.newFont("/fonts/carbon_bl.ttf",20)
+	carbon_font_40 = love.graphics.newFont("/fonts/carbon_bl.ttf",40)
+	carbon_font_60 = love.graphics.newFont("/fonts/carbon_bl.ttf",60)
+	carbon_font_80 = love.graphics.newFont("/fonts/carbon_bl.ttf",80)
 	alarm = love.graphics.newImage("/img/alarm.png")
 	alert = love.graphics.newImage("/img/alert.png")
 	cloud = love.graphics.newImage("/img/cloud.png")
@@ -36,11 +36,14 @@ function love.update(dt)
 end
  
 function love.draw()
-    view({w=40,h=100,x=0,y=0},data.name,carbon_font_20)
-    view({w=40,h=100,x=160,y=0},data.main.temp,carbon_font_20)
-    view({w=40,h=100,x=320,y=0},data.main. humidity,carbon_font_20)
-    view({w=40,h=100,x=480,y=0},data.wind.speed,carbon_font_20)
-    view({w=200,h=100,x=120,y=190},os.date("%X"),carbon_font_80)
-    view({w=100,h=100,x=400,y=190},os.date("%x"),carbon_font_60)
-    love.graphics.draw(sunny,300,300);
+    view({w=160,h=60,x=30,y=0},"Temp:"..(tonumber(data.main.temp)-273),carbon_font_20)
+    --view({w=160,h=60,x=30,y=0},data.name,carbon_font_20)
+    view({w=160,h=60,x=190,y=0},os.date("%x"),carbon_font_20)
+    --view({w=160,h=60,x=190,y=0},data.main.temp,carbon_font_20)
+    view({w=160,h=60,x=350,y=0},"Humidity:"..data.main.humidity.."%",carbon_font_20)
+    view({w=160,h=60,x=30,y=280},"Sbasu3",carbon_font_20)
+    view({w=160,h=60,x=190,y=280},"Wind:"..data.wind.speed.."KM/Hr",carbon_font_20)
+    view({w=160,h=60,x=350,y=280},"Clouds:"..data.clouds.all.."%",carbon_font_20)
+    view({w=280,h=140,x=200,y=90},os.date("%X"),carbon_font_80)
+    love.graphics.draw(sunny,30,90);
 end
